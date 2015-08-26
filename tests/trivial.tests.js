16ee033e9b7068e4c2e6ce1a23b6a97c75d300b4
SC <github.com@genkisugi.net>
2015-08-25 22:59:16 -0700

    allow recursive directory traversal at startup--makes for better plugin discovery for geneontology/noctua#178

--- tests/trivial.tests.js
+++ tests/trivial.tests.js
@@ -54,3 +54,18 @@ describe('trivial tests for file searching in the constructor', function(){
 		     'name: 1', 'name: 1');
     });
 });
+
+describe('recursive constructor', function(){
+
+    it('find all files, non-recursive', function(){
+    	var pup_tent = require('..')(['tests/rsrc3']);	
+    	//console.log(pup_tent.cached_list());
+    	assert.equal(pup_tent.cached_list().length, 8, 'two items x 4');
+    });
+
+    it('find all files, recursive', function(){
+	var pup_tent = require('..')(['tests/rsrc3'], null, true);	
+	//console.log(pup_tent.cached_list());
+	assert.equal(pup_tent.cached_list().length, 12, 'three items x 4');
+    });
+});
